779ff3cfc1db4be859300dac6cc777ce61690b57
Dmitry Likhaletov <likhaletov.d@gmail.com>
2020-08-01 21:50:52 +0300

    almost done

--- GIF Preview - Test task/View/CustomTableViewCell.swift
+++ GIF Preview - Test task/View/CustomTableViewCell.swift
@@ -30,7 +30,6 @@ class CustomTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
         setupCustomImageView()
         setupImageIdLabel()
     }
